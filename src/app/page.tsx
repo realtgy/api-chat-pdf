1e11728fa7d6dcb833b3c3f4f43c39ddb719b4d9
you tang <tangbob171@gmail.com>
2025-05-28 00:22:55 +0800

    apply customer global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <main className="flex  text-2xl text-lime-500 align-items-center h-screen">
-      <p>This is the Landing page!</p>
-
-      <Button className="bg-lime-500 text-white hover:bg-lime-600">
-        Click Me
-      </Button>
-    </main>
+    <div className="section-container">
+      <Button variant="orange">Click me </Button>
+    </div>
   );
 }
