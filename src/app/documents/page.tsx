ae19629a5c94e0db7d9bb170e3af0b0858ef03b8
you tang <tangbob171@gmail.com>
2025-05-28 21:35:07 +0800

    documents mock data

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -2,6 +2,23 @@ import { Button } from "@/components/ui/button";
 import { Pencil, File, Trash2, Upload } from "lucide-react";
 import Link from "next/link";
 export default function Documents() {
+  const documents = [
+    {
+      fileName: "User_Manual.pdf",
+      fileSize: "1 MB",
+      createAt: "yesterday",
+    },
+    {
+      fileName: "Learn Python.pdf",
+      fileSize: "7 MB",
+      createAt: "2 days ago",
+    },
+    {
+      fileName: "Google Financial Report.pdf",
+      fileSize: "1 MB",
+      createAt: "today",
+    },
+  ];
   return (
     <section className="bg-[#faf9f6] min-h-screen">
       <div className="section-container">
@@ -17,37 +34,49 @@ export default function Documents() {
         <div className="bg-white rounded shadow w-full overflow-x-scroll">
           <table className="min-w-full">
             <tbody>
-              <tr>
-                <td className="p-4 text-left flex items-center">
-                  <File className="w-4 h-4 mr-4" style={{ strokeWidth: "3" }} />
-                  <Link href="#">
-                    <span className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[300px] text-sm font-medium">
-                      Tonnny_Rbins.pdf
-                    </span>
-                  </Link>
-                </td>
-                <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
-                  1MB
-                </td>
+              {documents.map((file, index) => (
+                <tr
+                  key={index}
+                  className={
+                    index === documents.length - 1
+                      ? ""
+                      : "border-b border-gray-200"
+                  }
+                >
+                  <td className="p-4 text-left flex items-center">
+                    <File
+                      className="w-4 h-4 mr-4"
+                      style={{ strokeWidth: "3" }}
+                    />
+                    <Link href="#">
+                      <span className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[300px] text-sm font-medium">
+                        {file.fileName}
+                      </span>
+                    </Link>
+                  </td>
+                  <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
+                    {file.fileSize}
+                  </td>
 
-                <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
-                  3 days ago
-                </td>
+                  <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
+                    {file.createAt}
+                  </td>
 
-                <td className="p-4 text-right w-4">
-                  <Pencil
-                    className="w-4 h-4 cursor-pointer"
-                    style={{ strokeWidth: "3" }}
-                  />
-                </td>
+                  <td className="p-4 text-right w-4">
+                    <Pencil
+                      className="w-4 h-4 cursor-pointer"
+                      style={{ strokeWidth: "3" }}
+                    />
+                  </td>
 
-                <td className="p-4 text-right w-4">
-                  <Trash2
-                    className="w-4 h-4 cursor-pointer"
-                    style={{ strokeWidth: "3" }}
-                  />
-                </td>
-              </tr>
+                  <td className="p-4 text-right w-4">
+                    <Trash2
+                      className="w-4 h-4 cursor-pointer"
+                      style={{ strokeWidth: "3" }}
+                    />
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
