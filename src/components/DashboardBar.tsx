1862498930bd062424daba1670c97dcf82a930b1
you tang <tangbob171@gmail.com>
2025-05-30 15:09:16 +0800

    优化dashboard布局

--- src/components/DashboardBar.tsx
+++ src/components/DashboardBar.tsx
@@ -5,20 +5,16 @@ import { UserButton } from "@clerk/nextjs";
 export default function DashboardBar() {
   return (
     <header className="sticky left-0 top-0 z-50 bg-[#f8f5ee] w-full backdrop-blur border-slate-500/19">
-      <div className="max-auto h-[60px] max-w-7xl px-8 md:px-6">
-        <div className="flex items-center justify-between  border-red-600 h-full">
+      <div className="mx-auto h-[60px] max-w-7xl px-8 md:px-6">
+        <div className="flex items-center justify-between h-full">
           {/* left Icon */}
-          <div className="flex">
+          <div className="flex items-center">
             <BookOpenCheck className="text-black w-7 h-7 mr-3" />
             <span className="text-lg font-medium text-black">PDF.wisdom</span>
           </div>
 
-          {/* right user button ？？*/}
-          <div
-            style={{
-              border: "1px solid red",
-            }}
-          >
+          {/* right user button */}
+          <div className="flex items-center">
             <UserButton afterSignOutUrl="/" />
           </div>
         </div>
